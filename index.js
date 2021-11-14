747a8a60ec9f743a21c93fdf2ce03144226455f6
Eveline Guerrero <evelineguerrero23@gmail.com>
2021-11-13 22:13:48 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+    var cats = ["Milo", "Otis", "Garfield"];
+
+  function destructivelyAppendCat(name) {
+      cats.push(name);
+  }
+
+  function destructivelyPrependCat(name) {
+      cats.unshift(name);
+  }
+
+  function destructivelyRemoveLastCat(){
+      cats.pop();
+  }
+
+  function destructivelyRemoveFirstCat(){
+      cats.shift();
+  }
+
+  function appendCat(name){
+      var newArray = cats.slice();
+      newArray.push(name);
+      return newArray;
+  }
+
+  function prependCat(name){
+      let newArray = cats.slice();
+      newArray.unshift(name);
+      return newArray;
+  }
+
+  function removeLastCat(){
+    let newArray = cats.slice();
+    newArray.pop();
+    return newArray
+  }
+
+  function removeFirstCat(){
+    let newArray = cats.slice();
+    newArray.shift();
+return newArray;
+
+  }
+
+ 
